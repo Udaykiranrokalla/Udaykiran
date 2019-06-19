47a0f67d88224eb2d23310a7a64dd9ec1371ab1d
miracle <miracle@MC100071896.corp.miraclesoft.com>
2019-06-19 23:05:16 +0530

    commit

---
+++ rakeitg/src/rakeitg/add.java
@@ -0,0 +1,8 @@
+package rakeitg;
+
+public class add {
+ public static void main(String[]args)
+ {
+	 System.out.println("add");	 
+ }
+}
--- rakeitg/src/rakeitg/hellowld.java
+++ rakeitg/src/rakeitg/hellowld.java
@@ -4,6 +4,6 @@ public class hellowld
 {
 public static void main(String[]args) 
  {
- System.out.println("Hello world");	
+ System.out.println("Rakesh");	
  }
 }
