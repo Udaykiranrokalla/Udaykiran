02d2bcc97d2cecd34dec6a30eb25040abe521632
saradhi_s <saradhi566@gmail.com>
2019-06-19 23:06:03 +0530

    hey

--- firstproject/src/firstproject/demo.java
+++ firstproject/src/firstproject/demo.java
@@ -4,7 +4,7 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("a=3,b=9");
+    System.out.println("a=3,b=9 c=0");
 	}
 
 }
