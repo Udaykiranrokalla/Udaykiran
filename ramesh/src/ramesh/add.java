5ec6af70d40351c15be90683797a4bbcf03135a1
saradhi_s <saradhi566@gmail.com>
2019-06-19 23:06:48 +0530

    commit

---
+++ ramesh/src/ramesh/add.java
@@ -0,0 +1,14 @@
+package ramesh;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
