864847fab202f9a8d58337c9be564865c47a07a3
Vinod <vinodkumarpydi14@gmail.com>
2019-06-20 20:26:46 +0530

    hi

---
+++ LathaProj/src/Print.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter number");
+      int n=sc.nextInt();
+      System.out.println(n);
+      System.out.println("enter character");
+      char ch=sc.next().charAt(0);
+      System.out.println(ch);
+	}
+
+}
