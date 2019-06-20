42b6c380041c18da2d96e7c4a08d0cb57e6d57be
saradhi_s <saradhi566@gmail.com>
2019-06-20 23:07:55 +0530

    sds

---
+++ ramesh/src/ramesh/Multiplication.java
@@ -0,0 +1,17 @@
+package ramesh;
+import java.util.*;
+public class Multiplication {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n;
+		System.out.println("enter the number");
+		n=sc.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n+"*"+i+"="+(n*i));
+		}
+
+	}
+
+}
