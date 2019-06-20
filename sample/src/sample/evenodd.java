ffa3267e24c45d800f376a8e442fdba0dc2ea383
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-20 17:01:25 +0530

    cmit

---
+++ sample/src/sample/evenodd.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class evenodd {
+	public static void main (String args[]) {
+		int number=30;
+		if(number%2==0){
+			System.out.println("even number");
+		 
+		 }else{  
+			 
+		 System.out.println("odd number");
+		}
+		
+	}
+
+}
