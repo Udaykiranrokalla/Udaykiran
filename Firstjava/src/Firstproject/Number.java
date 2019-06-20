5cf3cbe384d494dd500b9f63bf5bcdd44a8b85eb
siva <siva.cs579@gmail.com>
2019-06-20 11:38:32 +0530

    Add numbers

---
+++ Firstjava/src/Firstproject/Number.java
@@ -0,0 +1,11 @@
+package Firstproject;
+
+public class Number {
+	public static void main(String args)
+	{
+		int num1=8,num2=5,sum;
+		sum=num1+num2;
+		System.out.println("sum of these numbers:"+sum);
+	}
+
+}
