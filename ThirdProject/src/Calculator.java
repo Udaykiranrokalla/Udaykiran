cc0a14061df1a8093b6dc3ae9494156d54c7470f
satyavati <ms.satyavati@gmail.com>
2019-06-20 23:50:24 +0530

    calculator

---
+++ ThirdProject/src/Calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Calculator {
+public static void main(String[] args) {
+	
+	Scanner scan =new Scanner(System.in);
+	System.out.println("enter two numbers");
+	double a= scan.nextInt();
+	double b= scan.nextInt();
+	
+	System.out.println("enter an operator(+,-,*,/):");
+	char operator = scan.next().charAt(0);
+	double res;
+	switch(operator)
+	{
+	case '+':
+		res = a+b;
+		System.out.println("addition is" +res);
+	case '-':
+		res=a-b;
+		System.out.println("subtraction is" +res);
+	case '*':
+		res=a*b;
+		System.out.println("multiplication is" +res);
+	case '/':
+	   res=a/b;
+	   System.out.print("division is" +res);
+    default:
+    	System.out.println("please enter given operand");
+		   
+	}
+	
+	
+}
+}
