904f3728769b2b5bf6112b843b4656fda4fc2f2a
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-20 22:58:53 +0530

    cmit

---
+++ sample/src/sample/multiply.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.util.Scanner;
+
+public class multiply {
+public static void main(String args[]) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter the number");
+int n=s.nextInt();
+for(int i=1; i <= 10; i++)
+{
+    System.out.println(n+" * "+i+" = "+n*i);
+}
+
+}
+}
+
+
+
+
