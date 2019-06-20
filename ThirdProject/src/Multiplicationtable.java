d160ba1afb17b05fce1dacc6eb053dc26b56dc2a
satyavati <ms.satyavati@gmail.com>
2019-06-21 00:57:57 +0530

    multiplication table

---
+++ ThirdProject/src/Multiplicationtable.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Multiplicationtable {
+public static void main(String args[])
+{
+	int n, i;
+	System.out.println("Enter an integer to print it's multiplication table");
+	Scanner scan = new Scanner(System.in);
+	n = scan.nextInt();
+	System.out.println("Multiplication table of " + n);
+	 
+	for (i=1;i<= 10;i++)
+	 System.out.println(n + "*" + i + " = " + (n*i));
+	 }
+	}
+
