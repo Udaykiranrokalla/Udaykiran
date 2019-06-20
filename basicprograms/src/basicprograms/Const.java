786b9235757f0096e6a29693c28bfd3d02438594
manojsrikanth <manojsrikanthmss@gmail.com>
2019-06-20 12:41:57 +0530

    c

---
+++ basicprograms/src/basicprograms/Const.java
@@ -0,0 +1,19 @@
+package basicprograms;
+
+class C{
+int i,j;
+C(int i,int j){
+	this.i=i;
+	this.j=j;
+	System.out.println(j*i);
+	
+}
+}
+public  class Const {
+	public static void main(String args[]) {
+		C a=new C(3,5);
+		
+		
+	}
+}
+
---
+++ basicprograms/src/basicprograms/Oops.java
@@ -0,0 +1,14 @@
+package basicprograms;
+
+import java.util.Arrays;
+
+public class Oops {
+public static void main(String args[]) {
+int[] arr= {1,2,3,4,56,7,8,9};
+Arrays.sort(arr);
+System.out.println(Arrays.toString(arr));
+
+	
+}
+
+}
