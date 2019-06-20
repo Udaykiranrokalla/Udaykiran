56f5c51c014a352c72d97741e817e0381f0464f1
satyavati <ms.satyavati@gmail.com>
2019-06-20 14:06:26 +0530

    hii

---
+++ ThirdProject/src/Uday/Length.java
@@ -0,0 +1,14 @@
+package Uday;
+
+public class Length {
+
+	public static void main(String args[]) throws Exception {
+		      String str = "uday kiran";
+		      int i = 0;
+		      for(char c: str.toCharArray()) {
+		         i++;
+		      }
+		      System.out.println("Length of the given string ::"+i);
+		   }
+		}
+
