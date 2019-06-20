50c624a3de4995a8f9d0f24a080bebade59b9673
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-20 23:41:33 +0530

    cmit

---
+++ sample/src/sample/square.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class square {
+	public static void main (String [] args)
+	{
+		int number;
+		int square;
+		
+		
+		System.out.println("Number\tSquare");
+		
+		
+		for (number=1; number<11; number++)
+		{
+			square= number * number;
+			
+			System.out.println(number+"\t"+square+"\t");
+			
+		}
+	}
+
+}
+
+
