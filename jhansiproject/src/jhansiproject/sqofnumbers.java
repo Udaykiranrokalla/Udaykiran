c9ff5f197d8a1d528baa2b060fe5b79cedd4d982
miracle <miracle@172.17.14.187>
2019-06-20 23:30:03 +0530

    new prog

---
+++ jhansiproject/src/jhansiproject/sqofnumbers.java
@@ -0,0 +1,14 @@
+package jhansiproject;
+import java.util.*;
+public class sqofnumbers {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+ for(int i=1;i<=20;i++)
+ {
+	 System.out.println("i*i");
+ }
+	}
+
+}
