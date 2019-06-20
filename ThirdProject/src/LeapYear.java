b369185c2d313dac10253c1b2acb5818838c292b
satyavati <ms.satyavati@gmail.com>
2019-06-20 17:45:59 +0530

    leap year

---
+++ ThirdProject/src/LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class LeapYear 
+{
+public static void main(String[] args)
+{
+	int year;
+	Scanner scan =new Scanner(System.in);
+	System.out.println("enter a year ");
+	year = scan.nextInt();
+	if(((year%4==0)&&(year%100==0))||(year%400==0)) {
+		System.out.println("leap year");
+		}
+	else  {
+		System.out.println("Not a leap year ");
+	}
+}
+}
+
