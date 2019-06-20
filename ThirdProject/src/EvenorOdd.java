5da80b8fb3da43c20f8a9d25e0f04555ff1cbee1
satyavati <ms.satyavati@gmail.com>
2019-06-20 17:31:08 +0530

    even or odd

---
+++ ThirdProject/src/EvenorOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class EvenorOdd 
+{
+public static void main(String[] args)
+{
+	double number;
+	Scanner scan =new Scanner(System.in);
+	System.out.println("enter a number ");
+	number = scan.nextInt();
+	if(number%2==0) {
+		System.out.println("number is even");
+		}
+	else  {
+		System.out.println("number is odd ");
+	}
+}
+}
+
