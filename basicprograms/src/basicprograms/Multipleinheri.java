baaa97d5136223d6a45b2b4f4679fcf658e2d401
manojsrikanth <manojsrikanthmss@gmail.com>
2019-06-20 17:42:30 +0530

    static programs

---
+++ basicprograms/src/basicprograms/Multipleinheri.java
@@ -0,0 +1,22 @@
+package basicprograms;
+
+class A{
+	int i,j;
+	void abc() {
+		System.out.println(i*j);
+	}
+	
+	
+}
+class B{
+	int c,d;
+	void abc() {
+		System.out.println(c*d);
+	}
+	
+}
+
+
+class Multipleinheri extends A {
+
+}
