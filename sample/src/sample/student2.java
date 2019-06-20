44506e57d251689b6e8cde6a9a526ac0cbc6ded9
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-20 16:51:56 +0530

    cmt

--- sample/src/sample/student2.java
+++ sample/src/sample/student2.java
@@ -6,22 +6,21 @@ public class student2 {
 	String name;//local variable//
 	
 	
-	Student2(int i,int a,String n){  
+	student2(int i,int a,String n){ 
 	    id = i;
 	    age=a;
 	    name = n;  
 	    }  
-	    Student2(){}  
+	student2(){}  
 	    void display(){
 	    	System.out.println(id+" "+age+" "+name);}
 	   
 	    public static void main(String args[]){  
-	    Student2 a = new Student2(14654781,80,"alekyaaa");  
-	    Student2 b = new Student2();  
-	    b.id=a.id;  
-	    b.name=a.name;  
+	    	student2 a = new student2(14654781,80,"alekyaaa");  
+	    	
+	
 	    a.display();  
-	    b.display();  
+	     
 	   }  
 	} 
 	
