47fa8560116a64f3f092d6fda363f8fc843a49b4
saradhi_s <saradhi566@gmail.com>
2019-06-20 12:55:19 +0530

    commit

--- ramesh/src/ramesh/Student.java
+++ ramesh/src/ramesh/Student.java
@@ -1,10 +1,29 @@
 package ramesh;
 
 class Student {
-int id,age,m1,m2m3
+int id,age,m1,m2,m3;
+String name;
+static String company;
+public Student(int a,int b,int c,int d,int e,String n)
+{
+	this.id=a;
+	this.age=b;
+	this.m1=c;
+	this.m2=d;
+	this.m3=e;
+	this.name=n;
+}
+void average()
+{
+	double z=0.0;
+	z=(this.m1+this.m2+this.m3)/3.0;
+	System.out.println("Average of the student"+this.name+"is"+z);
+}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Student s1=new Student(11,21,70,80,90,"abc");
+		s1.average();
+		System.out.println(s1.id);
+		
 	}
 
 }
--- ramesh/src/ramesh/large.java
+++ ramesh/src/ramesh/large.java
@@ -2,18 +2,20 @@ package ramesh;
 import java.util.*;
 public class large {
 	public static void main(String[] args) {
-		int l=0,s=10,r=0,n1=0;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter a number");
-		int n=sc.nextInt();
 		
-		while(n>0)
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a numbers");
+		int n=sc.nextInt();
+		int l=0,s=10,r=0;
+		
+		while(n>0) {
 			r=n%10;
-		if(n>l)
+		if(r>l)
 			l=r;
 		if(r<s)
 			s=r;
-		n=n/100;;
+		n=n/10;;
+		}
 
 System.out.println("the largest number is:"+l);
 System.out.println("the samllest number is"+s);
