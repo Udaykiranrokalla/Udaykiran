2f39e834741eb3d04c1ec41a6e3f7c0b5145ac20
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-20 17:14:13 +0530

    cmit

---
+++ sample/src/sample/upperlowercase.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class upperlowercase {
+	
+	public static void main(String args[]) {
+		
+		String s="syamala";
+		
+		System.out.println(s.toUpperCase());
+		
+		System.out.println(s.toLowerCase());
+		
+		System.out.println(s);
+		
+	}
+
+}
