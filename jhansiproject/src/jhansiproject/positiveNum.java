f122b40577b6c863a780f106322c0451b7ebea69
miracle <miracle@172.17.14.187>
2019-06-20 17:38:57 +0530

    new prog

---
+++ jhansiproject/src/jhansiproject/positiveNum.java
@@ -0,0 +1,19 @@
+package jhansiproject;
+import java.util.*;
+public class positiveNum {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+int n=sc.nextInt();
+if(n>0)
+{
+	System.out.println(n+ "is a positive number");
+}
+else
+ {
+	System.out.println(n+ "is negative number");
+ }
+}
+
+}
