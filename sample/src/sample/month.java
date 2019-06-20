9b3d24fb4a806c790dc0625e871f754db27ef9e6
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-20 20:44:34 +0530

    cmit

---
+++ sample/src/sample/month.java
@@ -0,0 +1,28 @@
+package sample;
+
+import java.util.Scanner;
+
+public class month {
+	public static void main(String args[]) {
+		int month;
+		Scanner s=new Scanner(System.in);
+		System.out.print(" Please Enter Month Number from 1 to 12 (1 = Jan, and 12 = Dec) : ");
+	    month=s.nextInt();
+	    
+	    if(month==1||month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 );
+	    {
+	    	System.out.println("the no of days in the month is 31");
+	    }
+	    if ( month == 4 || month == 6 || month == 9 || month == 11 )
+		{
+			System.out.println("\n 30 Days in this Month");  	
+		}  
+		else if(month==2)
+		{
+			System.out.println("\n Either 28 or 29 Days in this Month");  	
+	}
+
+	}
+	}
+
+
