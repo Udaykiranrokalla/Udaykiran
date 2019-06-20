bf9de95689735f1a0e9bd4e532f95210ea32eeaa
Vinod <vinodkumarpydi14@gmail.com>
2019-06-20 16:35:12 +0530

    hii

---
+++ LathaProj/src/DeclInt.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class DeclInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int i,j;
+         i=67;
+	}
+
+}
---
+++ LathaProj/src/PosOrNeg.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class PosOrNeg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Scanner sc=new Scanner(System.in);
+         System.out.println("enter number");
+         int a=sc.nextInt();
+         if(a>0) {
+        	 System.out.println(a+"is positive");
+         }
+         else {
+        	 System.out.println(a+"is negative");
+         }
+	}
+
+}
