8dc6e20a4878d105e41009afb9fa1bd93197590b
siva <siva.cs579@gmail.com>
2019-06-20 17:31:51 +0530

    three

---
+++ Firstjava/src/Firstproject/Divisibleby3or5.java
@@ -0,0 +1,20 @@
+package Firstproject;
+
+import java.util.Scanner;
+
+public class Divisibleby3or5 {
+	public static void main(String args[]) {
+		Scanner d=new Scanner(System.in);
+		int n;
+		System.out.println("enter a number");
+		n=d.nextInt();
+		if(n%3==0 && n%5==0)
+			System.out.println("the number is divisible by 3 and 5");
+			else
+			System.out.println("the number is not divisible by 3 and 5");
+
+			}
+			}
+		
+
+
