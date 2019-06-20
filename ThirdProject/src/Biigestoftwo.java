5b1fef71229a2fa0cc7f0bdeca26eaacc1e064ff
satyavati <ms.satyavati@gmail.com>
2019-06-20 22:38:52 +0530

    biggest

---
+++ ThirdProject/src/Biigestoftwo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Biigestoftwo {
+public static void main(String[] args) {
+	int a,b;
+	Scanner scan=new Scanner(System.in);
+	System.out.println("enter two numbers");
+	a=scan.nextInt();
+	b=scan.nextInt();
+	if(a>b) {
+		System.out.println( "a is big");
+	} else {
+		System.out.println("b is big");
+		}
+	}
+}
+
