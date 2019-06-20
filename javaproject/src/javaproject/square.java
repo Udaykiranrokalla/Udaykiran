3073b9523f71c2fd9787bfb2739c7479205adfa5
jeeva <venkatajeevakiran@gmail.com>
2019-06-21 01:03:48 +0530

    hii

---
+++ javaproject/src/javaproject/square.java
@@ -0,0 +1,12 @@
+package javaproject;
+
+import java.util.Scanner;
+
+public class square {
+	public static void main(String args[]) {
+		System.out.println("enter digit ");
+		Scanner obj=new Scanner(System.in);
+		int n=obj.nextInt();
+			 System.out.println("squre of number: " + n*n);
+	}
+}
