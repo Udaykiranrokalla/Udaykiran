9ca0751f58f242ddbd778b54c8c0879a9eeeb3ed
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-20 17:27:20 +0530

    cmit

---
+++ sample/src/sample/pstivenegtive.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.util.Scanner;
+
+public class pstivenegtive {
+	
+public static void main(String args[]) {
+	
+	int n;
+	
+			Scanner s=new Scanner(System.in);
+			
+	System.out.println("enter a number");
+	
+	
+	n=s.nextInt();
+	
+	if(n>0)
+	{
+	System.out.println("the given value on n is positive");
+	}
+	else
+	System.out.println("the given value of n is negative");
+	}
+
+	
+
+
+			
+}
+
+
