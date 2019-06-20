cbb444f987e052b733e4d531913f1ec935a07017
jeeva <venkatajeevakiran@gmail.com>
2019-06-21 00:14:37 +0530

    hiii

---
+++ javaproject/src/javaproject/choice.java
@@ -0,0 +1,41 @@
+package javaproject;
+
+import java.util.Scanner;
+
+public class choice {
+	public static void main(String args[]) {
+		System.out.println("enter digit ");
+		Scanner obj=new Scanner(System.in);
+		int i=obj.nextInt();
+		switch(i) {
+		case 1:
+			System.out.println("one ");
+			break;
+		case 2:
+			System.out.println("two ");
+			break;
+		case 3:
+			System.out.println("three ");
+			break;
+		case 4:
+			System.out.println("four ");
+			break;
+		case 5:
+			System.out.println("five ");
+			break;
+		case 6:
+			break;
+		case 7:
+			System.out.println("seven ");
+			break;
+		case 8:
+			System.out.println("eight");
+			break;
+		case 9:
+			System.out.println("nine ");
+			break;
+			
+			
+		}
+	}
+}
