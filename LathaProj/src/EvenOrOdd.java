52615a4faa879c2c1e327132aa80f62d6911847f
Vinod <vinodkumarpydi14@gmail.com>
2019-06-20 16:40:35 +0530

    hii

---
+++ LathaProj/src/EvenOrOdd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter number");
+       int n=sc.nextInt();
+       if(n%2==0)
+       {
+    	   System.out.println("even");
+       }
+       else
+       {
+    	   System.out.println("odd");
+       }
+	}
+
+}
