24ae4766f1b38fc725a1f155b93c30e0a12d2710
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 10:45:30 +0530

    cmit

---
+++ sample/src/sample/whi.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.util.Scanner;
+
+public class whi {
+public static void main (String args[]) {
+	int i;
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter the number");
+i=s.nextInt();
+while(i>1);
+{
+	if(i%2==0)
+	{
+			System.out.println(i+"");
+			i++;
+	}
+}
+}
+}
