cf8432119d73de1f833b948645c1a9fd6bc0dec8
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 19:47:46 +0530

    cmit

---
+++ sample/src/sample/startendwith.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class startendwith {
+public static void main(String args[]) {
+	String s="syamala";
+	System.out.println(s.startsWith("s"));
+	System.out.println(s.endsWith("a"));
+}
+}
