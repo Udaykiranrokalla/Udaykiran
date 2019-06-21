c86dfb046907e92d71e7ae3f8c724f7673182d05
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 20:10:37 +0530

    cmitt

---
+++ sample/src/sample/comrto.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class comrto {
+public static void main(String args[]) {
+	Integer i=6;
+	System.out.println(i.compareTo(2));
+	System.out.println(i.compareTo(6));
+	System.out.println(i.compareTo(7));
+}
+}
