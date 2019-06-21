8d7ce4fec9bd6e35d719597d5ccbb2060a31aa13
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 19:57:17 +0530

    cmit

---
+++ sample/src/sample/index.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class index {
+public static void main(String args[]) {
+	String s="sam apple syamala";
+	System.out.println(s.indexOf('a'));
+	System.out.println(s.indexOf('e'));
+	System.out.println(s.indexOf('l'));
+}
+}
