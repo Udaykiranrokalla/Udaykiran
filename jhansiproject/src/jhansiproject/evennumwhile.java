617d1a40c46116ab848f235939429dc9a964e82d
miracle <miracle@172.17.14.187>
2019-06-21 20:23:13 +0530

    hiii

---
+++ jhansiproject/src/jhansiproject/evennumwhile.java
@@ -0,0 +1,24 @@
+package jhansiproject;
+import java.util.*;
+public class evennumwhile {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+int n=sc.nextInt();
+int no=1;
+System.out.println("even number");
+while(no!=n)
+{
+	if(no%2==0)
+	{
+		System.out.println(no);
+		}
+          no++;
+}
+}
+}
+
+
+
+
