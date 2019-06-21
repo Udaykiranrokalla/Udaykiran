0f94c35a78f3dc34e4101b2c996fe99667726aad
siva <siva.cs579@gmail.com>
2019-06-21 11:48:40 +0530

    atoz

---
+++ Firstjava/src/Firstproject/Charactersatoz.java
@@ -0,0 +1,12 @@
+package Firstproject;
+
+public class Charactersatoz {
+	public static void main(String arg[]) {
+		char ch;
+		for(ch='a';ch<='z';ch++) {
+			System.out.println(ch);
+		}
+		
+	}
+
+}
