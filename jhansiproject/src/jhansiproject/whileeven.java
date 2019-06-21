35e3a4a1e5a58aa9a85aa00811b82d2a0b15fee8
miracle <miracle@172.17.14.187>
2019-06-21 19:52:37 +0530

    hii

---
+++ jhansiproject/src/jhansiproject/whileeven.java
@@ -0,0 +1,18 @@
+package jhansiproject;
+import java.util.*;
+public class whileeven {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+int n=sc.nextInt();
+for(int i=1;i<n;i++)
+{
+	if(i%2==0)
+	{
+		System.out.println(i);
+	}
+}
+	}
+
+}
