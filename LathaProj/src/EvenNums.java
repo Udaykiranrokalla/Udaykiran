0ed8fe97e4823394f040865cfb461866430504d1
Vinod <vinodkumarpydi14@gmail.com>
2019-06-21 12:35:50 +0530

    hii

---
+++ LathaProj/src/EvenNums.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class EvenNums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=1;i<=n;i++) {
+    	 if(i%2==0) {
+    		 System.out.println(i);
+    	 }
+     }
+	}
+
+}
