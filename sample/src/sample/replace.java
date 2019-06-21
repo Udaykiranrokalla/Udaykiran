5011ee9be82b7a3385bd08d058dd44b417b33c58
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 20:24:45 +0530

    cmit

---
+++ sample/src/sample/replace.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class replace {
+	
+public static void main(String args[]) {
+	
+	StringBuffer l=new StringBuffer("samu");
+	
+	l.replace(1,3,"apple");
+	
+	System.out.println(l);
+	}
+	
+}
+
