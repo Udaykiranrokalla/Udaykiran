32de12dbbad292d439e04fb9f692234459b36724
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 20:33:34 +0530

    cmit

---
+++ sample/src/sample/reverse.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class reverse {
+public static void main(String args[]) {
+	StringBuffer sb=new StringBuffer("syamala");
+	sb.reverse();
+	System.out.println(sb);
+	
+}
+}
