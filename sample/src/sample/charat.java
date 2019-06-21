a335ba7e6cda223ca64b56a15132fca884c4d5a1
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 19:20:50 +0530

    cmit

---
+++ sample/src/sample/charat.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class charat {
+public static void main(String args[]) {
+	String s="syamuisagudgirl";
+	System.out.println(s.charAt(4));
+	System.out.println(s.charAt(8));
+}
+}
