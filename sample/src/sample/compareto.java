1f36193421d04721118bfd25b01b1cf01aaac8ff
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 20:03:11 +0530

    cmit

---
+++ sample/src/sample/compareto.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class compareto {
+public static void main(String args[]) {
+	String a="SYAMU";
+	String b="syamu";
+	if(a.equals(b)){
+		System.out.println("Both strings are equal.");
+		} else {
+		System.out.println("Both strings are not equal.");
+		}
+}
+
+}
