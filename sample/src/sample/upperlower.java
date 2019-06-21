ec3fdf207819012137d9cb062b74f349cd0ad3ee
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-21 18:52:52 +0530

    cmit

---
+++ sample/src/sample/upperlower.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class upperlower {
+public static void main(String args[]) {
+	String s="syamu";
+	System.out.println(s.toUpperCase());
+	System.out.println(s.toLowerCase());
+}
+}
