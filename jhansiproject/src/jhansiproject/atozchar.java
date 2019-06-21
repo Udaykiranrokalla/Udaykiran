97417019d46ca477b374aba708192033f4357b23
miracle <miracle@172.17.14.187>
2019-06-21 19:44:57 +0530

    new

---
+++ jhansiproject/src/jhansiproject/atozchar.java
@@ -0,0 +1,17 @@
+package jhansiproject;
+import java.util.*;
+import java.util.Scanner;
+public class atozchar {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a to z characters");
+char ch;
+for(ch='a';ch<='z';ch++)
+{
+	System.out.println(ch);
+}
+	}
+	
+
+}
