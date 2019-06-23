96e9e81b5be570519c54ad94cb49c9b429db99ae
satyavati <ms.satyavati@gmail.com>
2019-06-23 13:02:13 +0530

    hii

--- ThirdProject/src/Constuctor/Constructor2.java
+++ ThirdProject/src/Constuctor/Constructor2.java
@@ -1,28 +1,41 @@
 package Constuctor;
 
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
 public class Constructor2 {
-	int id;
+	int Eid;
 	String name;
 	int no;
 	int number;
-	Constructor2(int Id,String Name) {
-		this.id=Id;
-		this.name=Name;
+	Constructor2() {
+		this(1,3);
+		
 	}
 	Constructor2(int Eid, int Number) {
-		this.no=Eid;
-		this.number=Number;
-       // Constructor2(1, "uday");
+		this(Eid,6,"dhoni");
+		this.Eid=Eid;
+		this.no=Number;
+		
 	}
+	Constructor2(int Eid, int Number,String name) {
+		this.Eid=Eid;
+		this.no=Number;
+		this.name=name;
+	}
+	
 	void display()
 	{
-		System.out.println(no + " " + number);
-	
+		System.out.println(Eid + " " + name);
 	}
+	void display1() {
+		System.out.println(no +" " +number);
+   }
 	public static void main(String[] args) {
 		
-        Constructor2 ob1=new Constructor2(123,545);
+        Constructor2 ob1=new Constructor2();
+        //Constructor2 ob2=new Constructor2(111,456);
 	    ob1.display();
+	    ob1.display1();
 	    
 	}
 }
