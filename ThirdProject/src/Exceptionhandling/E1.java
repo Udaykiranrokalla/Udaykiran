2cc222ba345e3654395438b3b55b2312328be29c
satyavati <ms.satyavati@gmail.com>
2019-06-23 14:45:40 +0530

    exception

---
+++ ThirdProject/src/Exceptionhandling/E1.java
@@ -0,0 +1,14 @@
+package Exceptionhandling;
+
+public class E1 {
+public static void main(String args[]){  
+	   try{ 
+	      int n1=100/0; 
+	   }
+	   catch(Exception e)
+	   {
+		   System.out.println(e);
+		   }   
+	   System.out.println("uday kiran");  
+	  }  
+	}  
