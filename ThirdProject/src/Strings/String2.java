7173e58933d4151341496e55a1ce4daa9e535865
satyavati <ms.satyavati@gmail.com>
2019-06-23 11:19:10 +0530

    hii

---
+++ ThirdProject/src/Strings/String2.java
@@ -0,0 +1,12 @@
+package Strings;
+class String2{  
+	 public static void main(String args[]){  
+	   String s1="uday";  
+	   String s2="uday";  
+	   String s3=new String("uday");  
+	   String s4="kiran";  
+	   System.out.println(s1.equals(s2));
+	   System.out.println(s1.equals(s3)); 
+	   System.out.println(s1.equals(s4));  
+	 }  
+	}  
