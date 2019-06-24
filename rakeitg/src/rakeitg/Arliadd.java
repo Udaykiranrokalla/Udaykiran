72aa108c7f90a8b4ef2687ce278545761dbabed6
miracle <miracle@MC100071896.corp.miraclesoft.com>
2019-06-24 19:25:41 +0530

    committed

---
+++ rakeitg/src/rakeitg/Arliadd.java
@@ -0,0 +1,15 @@
+package rakeitg;
+import java.util.*;
+public class Arliadd {
+	public static void main(String[]args)
+	{
+		ArrayList<String> quan=new ArrayList<String>();
+		quan.add("Tiger");
+		quan.add("Lion");
+		quan.add("Elephant");
+		quan.add("Cheetah");
+		
+		System.out.println(quan);
+	}
+
+}
