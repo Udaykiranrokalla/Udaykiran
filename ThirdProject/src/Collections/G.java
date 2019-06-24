bdcd50005ca4a513398f566a1a143c27c4fc422b
satyavati <ms.satyavati@gmail.com>
2019-06-24 19:44:31 +0530

    collections

---
+++ ThirdProject/src/Collections/G.java
@@ -0,0 +1,22 @@
+package Collections;
+
+import java.util.*;
+
+public class G {
+public static void main(String args[]) {
+	ArrayList al =new ArrayList();
+al.add("uday");
+al.add(2);
+al.add('c');
+al.add(1,"kiran");
+al.add(0,"UK");
+System.out.println("elements are" +al);
+ArrayList bl =new ArrayList();
+bl.add("uday");
+bl.add(8);
+bl.add('e');
+System.out.println("elements are" +bl);
+al.retainAll(bl);
+System.out.println("retainall elements" +al);
+}
+}
