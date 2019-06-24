fc15c3ea538869bafab2afaf0707872608e65bf2
miracle <miracle@MC100071884.corp.miraclesoft.com>
2019-06-25 01:13:26 +0530

    last push @ 1:13 a.m

---
+++ projectjava/src/collections/List2set.java
@@ -0,0 +1,27 @@
+package collections;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class List2set {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> s1=new ArrayList<Integer>();
+		s1.add(507);
+		s1.add(508);
+		s1.add(518);
+		s1.add(582);
+		HashSet<Integer> s2=new HashSet<Integer>();
+	    for(Integer i:s1)
+	    	s2.add(i);
+	    Iterator i=s2.iterator();
+	    while(i.hasNext())
+		   {
+	        System.out.println(i.next());
+		   }
+
+		
+
+	}
+
+}
---
+++ projectjava/src/collections/Set2List.java
@@ -0,0 +1,28 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Set2List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String> s1=new HashSet<String>();
+		s1.add("harish");
+		s1.add("koti");
+		s1.add("uday");
+		s1.add("nikhil");
+		ArrayList<String> s2=new ArrayList<String>();
+	    for(String s:s1)
+	    	s2.add(s);
+	    Iterator i=s2.iterator();
+	    while(i.hasNext())
+		   {
+	        System.out.println(i.next());
+		   }
+
+
+	}
+
+}
