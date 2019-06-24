3575edc249cf748269c0a184ece05a6ddcfd0a41
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-24 19:42:50 +0530

    cmit

---
+++ sample/src/in/H.java
@@ -0,0 +1,15 @@
+package in;
+import java.util.*;
+public class H {
+public static void main(String args[]) {
+	HashSet<String> st=new HashSet<String>();
+	st.add("sam");
+st.add("sailu");
+	  st.add("lalii");
+	  st.add("lise");
+	  st.add("lucky");
+	  Iterator<String> itr=st.iterator();
+	  while(itr.hasNext());
+	  System.out.println(itr.next());
+}
+}
