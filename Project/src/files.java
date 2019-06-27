44e035fc2e8f27b084f0f6444fb5f2ee49bda7b3
miracle <miracle@MC100071889.corp.miraclesoft.com>
2019-06-27 15:40:00 +0530

    fileoutput

---
+++ Project/src/files.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class files {
+	public static void main(String args[]) {
+		try {
+			FileOutputStream fout=new FileOutputStream("E:\\koti1.txt");    
+			 fout.write(65);    
+			 fout.close();
+             System.out.println("success...");    
+            }catch(Exception e){System.out.println(e);}
+		}
+	}
+
