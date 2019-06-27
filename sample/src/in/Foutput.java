fc89cba1d604323e920e6e0e01d1fc118e384a59
miracle <miracle@MC21.corp.miraclesoft.com>
2019-06-27 10:48:56 +0530

    cmit

--- sample/src/in/Foutput.java
+++ sample/src/in/Foutput.java
@@ -4,9 +4,12 @@ public class Foutput {
 public static void main(String args[]) {
 	try {
 	FileOutputStream sm=new FileOutputStream("D://syam.txt");
+	
 	sm.write(75);
 	sm.close();
+	
 	System.out.println("valueabe");
+	
 	}catch(Exception e){System.out.println(e);}
 	
 	
