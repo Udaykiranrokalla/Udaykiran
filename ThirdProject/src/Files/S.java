4554466b459a2641938fb129e870db1aba388070
satyavati <ms.satyavati@gmail.com>
2019-06-28 00:41:14 +0530

    de serialisation

--- ThirdProject/src/Files/S.java
+++ ThirdProject/src/Files/S.java
@@ -1,21 +1,26 @@
-
+package src.Files;
 import java.io.*;
 public class S implements Serializable {
-	int id;
-String name;
+	static int id;
+static String name;
 public S(int id,String name) {
 	this.id=id;
 	this.name=name;
 }
-
-
 
 	public static void main(String args[]) throws Exception {
 		S s=new S(1,"uday");
 	FileOutputStream fout=new FileOutputStream("E:\\kiran\\kiran5.txt");
 	ObjectOutputStream o=new ObjectOutputStream(fout);
 	o.writeObject(s);
-	//System.out.print("go");
-	
+	FileInputStream fi=new FileInputStream("E:\\kiran\\kiran5.txt");
+	ObjectInputStream out=new ObjectInputStream(fi);
+	S s1 =(S)out.readObject();
+//s1.clone();
+fi.close();
+System.out.println("Object has been De-serialized"); 
+System.out.println("id" +id);   
+System.out.println("name:" +name); 
+
 	}
 }
