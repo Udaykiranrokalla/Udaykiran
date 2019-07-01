30d511de06fb9705e56dcdb24b1cd47fa0e92ce6
miracle <miracle@MC21.corp.miraclesoft.com>
2019-07-01 20:09:08 +0530

    cdwe

--- jdbc/src/jdbc/Cricket.java
+++ jdbc/src/jdbc/Cricket.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+
 public class Cricket {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
