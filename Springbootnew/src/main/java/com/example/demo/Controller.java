7883bea520c2b3432795c7a5a5cb4c75630a563d
manojsrikanth <manojsrikanthmss@gmail.com>
2019-07-26 12:44:21 +0530

    commit

--- Springbootnew/src/main/java/com/example/demo/Controller.java
+++ Springbootnew/src/main/java/com/example/demo/Controller.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import java.awt.PageAttributes.MediaType;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +27,31 @@ public String add(@RequestBody Pojo i) {
 		return "not entered";
 	}
 }
+
+@RequestMapping(value="/update/{id}",method=RequestMethod.POST,produces= {org.springframework.http.MediaType.APPLICATION_JSON_VALUE})
+@ResponseBody
+public String update(@RequestBody Pojo i,@PathVariable int id) {
+	int k=s.update(i,id);
+	System.out.println("i value ====>"+k);
+	return "data updated";
+	
+	
 }
+@RequestMapping(value="/delete/{id}",method=RequestMethod.POST,produces={org.springframework.http.MediaType.APPLICATION_JSON_VALUE})
+@ResponseBody
+public String delete(@PathVariable int id) {
+	int q=s.delete(id);
+	System.out.println("i value==>"+q);
+	return "deleted";
+	}
+
+@RequestMapping(value="/getdetails",method=RequestMethod.GET,produces= {org.springframework.http.MediaType.APPLICATION_JSON_VALUE})
+@ResponseBody
+public List<Pojo>getiteams(){
+	return s.getdetails();
+}
+
+}
+
+
+
--- Springbootnew/src/main/java/com/example/demo/Second.java
+++ Springbootnew/src/main/java/com/example/demo/Second.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -17,4 +19,27 @@ public int adddetails(Pojo f) {
 	String query="insert into employ values (?,?)";
 	return j.update(query,id,name);
 }
+
+
+public int update(Pojo f,int id) {
+   int result=0;
+   String sql="update employ  set name=? where id=?";
+   result=j.update(sql,f.getName(),id);
+   System.out.println("result"+result);
+   return result;
+
+		
+	}
+public int delete(int id) {
+	String query="delete from employ where id=?";
+	return j.update(query,id);
 }
+public List<Pojo>getdetails(){
+	List<Pojo> l=j.query("select * from employ",(result,rowNum)->new Pojo(result.getInt("id"),result.getString("name")));
+return l;
+
+
+}
+	
+}
+
