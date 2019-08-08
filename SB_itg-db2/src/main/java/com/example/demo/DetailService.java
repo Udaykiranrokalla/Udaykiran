7f4b7ede95491e05fbf50bcb752daf0899a5875b
sprathi <sprathi@MC50071713.corp.miraclesoft.com>
2019-08-09 00:22:28 +0530

    DB2

---
+++ SB_itg-db2/src/main/java/com/example/demo/DetailService.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+
+
+
+@Repository
+public class DetailService {
+	@Autowired
+	JdbcTemplate jdbt;
+	
+	
+	
+	public List<Details> getDetails() {
+		List<Details> details = jdbt.query("select * from spring", (result, rowNum) -> new Details(result.getInt("id"),
+				result.getString("name"),result.getInt("age")));
+		return details;
+
+	}
+	
+	
+	
+	
+	public int addDetails(Details d) {
+		int id = d.getId();
+		String name = d.getName();
+		int age=d.getAge();
+		String query = "INSERT INTO SPRING VALUES(?,?,?)";
+		return jdbt.update(query, id, name, age);
+	}
+}
---
+++ SB_itg-db2/src/main/java/com/example/demo/SbItgDb2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbItgDb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbItgDb2Application.class, args);
+	}
+
+}
